250b400632ebce81d27388a0ed3da9a84f10e4da
orhannozturkk <fzt.orhanozturk@gmail.com>
2024-04-02 15:19:51 +0300

    44

--- script.js
+++ script.js
@@ -14,3 +14,26 @@ function openDay(evt, salonName) {
     // Show the current tab, and add an "active" class to the link that opened the tab
     document.getElementById(salonName).style.display = "block";
     evt.currentTarget.className += " active";}
+
+// Get the modal
+var modal = document.getElementById('announcementModal');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the page loads, open the modal
+window.onload = function() {
+modal.style.display = "block";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
